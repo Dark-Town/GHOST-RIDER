730327cc75c2eb2fafbd23d66bff04a9d1788ce8
TCRONEB HACKX <thelastcroneb@gmail.com>
2024-12-26 09:44:39 +0100

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -13,7 +13,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let status = `*╭──────────●●►*
-*AWAIS MD UPTIME LIST↷*
+*GHOST-RIDER UPTIME LIST↷*
 
 *_UPTIME:➠_*  ${runtime(process.uptime())}
 
