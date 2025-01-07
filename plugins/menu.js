44eae608f63ecc4daaf99da365924e395f2668fb
THEOPHILUS739 <mashingaidzetheoohilus@gmail.com>
2025-01-08 01:10:10 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -93,7 +93,7 @@ let handler = async (m, { conn, usedPrefix }) => {
                                 "name": "cta_url",
                                 "buttonParamsJson": JSON.stringify({
                                     "display_text": "SCRIPT 💕",
-                                    "url": "https://github.com/GlobalTechInfo/ULTRA-MD"
+                                    "url": "https://github.com/Dark-Town/GHOST-RIDER"
                                 })
                             }
                         ],
