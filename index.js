6e644c7b0cf090048ed117fa89847ef043b60106
LiL CRONE <zwcoder263@gmail.com>
2025-02-05 14:35:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
-const client = require('./lib/client')
+const fs = require("fs").promises;
+const fsx = require("fs");
+const path = require("path");
+const config = require("./config");
+const connect = require("./lib/connection");
+const { getandRequirePlugins } = require("./lib/db/plugins");
+const { UpdateLocal, WriteSession, overrideConsoleLogs } = require("./lib");
 
-const connect = async () => {
-	try {
-		await client.connect()
-	} catch (error) {
-		console.error(error)
-	}
+global.__basedir = __dirname;
+
+process.on('message', (data) => {
+    if (data.type === 'overrideLogs') {
+        // Override console methods in the worker process
+        overrideConsoleLogs();
+    }
+});
+
+async function auth() {
+  try {
+    if (!fsx.existsSync("./lib/session/creds.json")) {
+      await WriteSession();
+    }
+    return initialize();
+  } catch (error) {
+    console.error("AuthFile Generation Error:", error);
+    return process.exit(1);
+  }
 }
 
-connect()
+const readAndRequireFiles = async (directory) => {
+  try {
+    const files = await fs.readdir(directory);
+    return Promise.all(
+      files
+        .filter((file) => path.extname(file).toLowerCase() === ".js")
+        .map((file) => require(path.join(directory, file)))
+    );
+  } catch (error) {
+    console.error("Error reading and requiring files:", error);
+    throw error;
+  }
+};
+
+async function initialize() {
+  console.log("============> GHOST-RIDER [TcroneB HackX] <============");
+  try {
+    await readAndRequireFiles(path.join(__dirname, "/lib/db/"));
+    console.log("Syncing Database");
+    await config.DATABASE.sync();
+    console.log("⬇  Installing Plugins...");
+    await readAndRequireFiles(path.join(__dirname, "/plugins/"));
+    await getandRequirePlugins();
+    console.log("✅ Plugins Installed!");
+    return  await connect();
+  } catch (error) {
+    console.error("Initialization error:", error);
+    return process.exit(1); // Exit with error status
+  }
+}
+
+auth();
