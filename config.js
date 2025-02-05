c2a2819a9c42ab28b65a9a9e0bb552fabd75c6fa
LiL CRONE <zwcoder263@gmail.com>
2025-02-05 14:40:08 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,69 +1,57 @@
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
+const { Sequelize } = require("sequelize");
+const { existsSync } = require('fs')
+const path = require('path')
+const configPath = path.join(__dirname, './config.env')
+if (existsSync(configPath)) require('dotenv').config({ path: configPath })
+const toBool = (x) => x === "true";
+const DATABASE_URL = process.env.DATABASE_URL || "./lib/db/database.db";
 
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env', override: true });
-
-const convertToBool = (text, fault = 'true') => text === fault;
-const toBool = (x) => (x && x.toLowerCase() === 'true') || false;
-
-global.apikey = { 'https://api.adithyan.xyz': 'free' };
-global.apiUrl = 'https://hermit-api.koyeb.app/';
-
-const DATABASE_URL = process.env.DATABASE_URL || './database.db';
-process.env.NODE_OPTIONS = '--max_old_space_size=2560';
-
-const DEBUG = convertToBool(process.env.DEBUG, 'true');
-
-module.exports = {
-  VERSION: 'v4.4.4',
-  SESSION_ID: process.env.SESSION_ID || '',
-  MODE: (process.env.MODE || 'private').toLowerCase(),
-  HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-  SEND_READ: toBool(process.env.READ_COMMAND),
-  READ_MSG: toBool(process.env.READ_MSG),
-  MSG_LOG: convertToBool(process.env.LOG_MSG),
-  BLOCKCHAT: process.env.BLOCK_CHAT || false,
-  LANG: (process.env.LANGUAGE || 'EN').toUpperCase(),
-  ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
-  BOT_NAME: process.env.BOT_NAME || 'GHOST-RIDER',
-  AUTOMUTE_MSG: process.env.AUTOMUTE_MSG || '_Group automuted!_\n_(Change this by setting var AUTOMUTE_MSG)_',
-  AUTOUNMUTE_MSG: process.env.AUTOUNMUTE_MSG || '_Group autounmuted!_\n_(Change this by setting var AUTOUNMUTE_MSG)_',
-  ANTILINK_MSG: process.env.ANTILINK_MSG || '_Link Not Allowed!_\n_(Change this by setting var ANTILINK_MSG)_',
-  BOT_INFO: process.env.BOT_INFO || 'GHOST-RIDER;https://i.imgur.com/6oRG106.jpeg',
-  AUDIO_DATA: process.env.AUDIO_DATA || 'ʜᴇʀᴍɪᴛ;ᴀᴅɪᴛʜyᴀɴ;https://i.imgur.com/fj2WE83.jpeg',
-  STICKER_DATA: process.env.STICKER_DATA || 'ʜᴇʀᴍɪᴛ;ᴀᴅɪᴛʜyᴀɴ',
-  ERROR_MESSAGE: toBool(process.env.ERROR_MESSAGE, 'true'),
-  SONG_THUMBNAIL: toBool(process.env.SONG_THUMBNAIL),
-  WARN: process.env.WARN || '4',
-  REJECT_CALL: toBool(process.env.REJECT_CALL),
-  KOYEB_API_KEY: process.env.KOYEB_API_KEY || false,
-  KOYEB_APP_NAME: process.env.KOYEB_APP_NAME || '',
-  RENDER_API: process.env.RENDER_API || false,
-  RENDER_NAME: process.env.RENDER_NAME || '',
-  TERMUX_VPS: toBool(process.env.TERMUX || process.env.VPS),
-  AUTO_STATUS_VIEW: toBool(process.env.AUTO_STATUS_VIEW),
-  APIKEY: process.env.APIKEY || 'free',
-  AUTH_FILE: process.env.AUTH_FILE || false,
-  START_MSG: toBool(process.env.START_MSG || 'true'),
+module.exports = {  
+  BASE_URL : "https://dxmods.xyz/",
+  API_KEY: process.env.API_KEY || "L5Ce7iyZng", //login here and get api key https://dxmods.xyz/login
+  SESSION_VALIDATOR : "https://dxmods.xyz/",
+  ANTILINK: toBool(process.env.ANTI_LINK) || false,
+  LOGS: toBool(process.env.LOGS) || true,
+  ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
+  SESSION_ID: process.env.SESSION_ID === undefined ? "" : process.env.SESSION_ID,
+  LANG: process.env.LANG || "EN",
+  AUTH_TOKEN: "",
+  HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null"  || process.env.HANDLER === undefined ? "^" : process.env.HANDLER,
+  RMBG_KEY: process.env.RMBG_KEY || false,
+  BRANCH:process.env.WARN_COUNT || "master",
+  WARN_COUNT:process.env.WARN_COUNT || 3,
+  WELCOME_MSG: process.env.WELCOME_MSG || "Hi @user Welcome to @gname",
+  GOODBYE_MSG: process.env.GOODBYE_MSG || "@user Left The Group",
+  PACKNAME: process.env.PACKNAME || "GHOST-RIDER",
+  AUTHOR: process.env.AUTHOR || "GHOST-RIDER",
+  SUDO: process.env.SUDO || "263716394979",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  OWNER_NAME: process.env.OWNER_NAME || "TcroneB HackX",
+  HEROKU: toBool(process.env.HEROKU) || false,
+  BOT_NAME: process.env.BOT_NAME || "GHOST-RIDER",
+  BOT_IMG : "https://envs.sh/8rA.jpg",
+  WORK_TYPE: process.env.WORK_TYPE || "public",
+  SESSION_URL: process.env.SESSION_URL || "",
+  DELETED_LOG_CHAT: "120363084228202932@g.us",
+  DELETED_LOG: toBool(process.env.DELETED_LOG) || false,
+  GEMINI_API: process.env.GEMINI_API === undefined ? false :  process.env.GEMINI_API,
   DATABASE_URL: DATABASE_URL,
-  DATABASE: DATABASE_URL === './database.db' 
-    ? new Sequelize({
-        dialect: 'sqlite',
-        storage: DATABASE_URL,
-        logging: false,
-      }) 
-    : new Sequelize(DATABASE_URL, {
-        dialect: 'postgres',
-        ssl: true,
-        protocol: 'postgres',
-        dialectOptions: {
-          native: true,
-          ssl: { require: true, rejectUnauthorized: false },
-        },
-        logging: false,
-      }),
-  RBG_API_KEY: process.env.REMOVE_BG_API_KEY || false,
-  BRAIN_ID: process.env.BRAIN_ID || 'bid=168613&key=EfbnX54Iy9PFIFp3',
-  SUDO: process.env.SUDO || '0,0',
-  DEBUG: DEBUG
+  DATABASE:
+    DATABASE_URL === "./lib/db/database.db"
+      ? new Sequelize({
+          dialect: "sqlite",
+          storage: DATABASE_URL,
+          logging: false,
+        })
+      : new Sequelize(DATABASE_URL, {
+          dialect: "postgres",
+          ssl: true,
+          protocol: "postgres",
+          dialectOptions: {
+            native: true,
+            ssl: { require: true, rejectUnauthorized: false },
+          },
+          logging: false,
+        }),
 };
